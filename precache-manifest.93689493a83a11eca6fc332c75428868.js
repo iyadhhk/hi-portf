efca3629af8943aacce96156eb4ad55ae1e5fb8d
unknown <iyadhbox@gmail.com>
2020-10-14 16:10:52 +0100

    Updates

---
+++ precache-manifest.93689493a83a11eca6fc332c75428868.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b03e3820593ba68101524696399fe9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d43ed217cf91cdca382a",
+    "url": "/static/css/main.f1744196.chunk.css"
+  },
+  {
+    "revision": "f1ad475b94662d8b930d",
+    "url": "/static/js/2.3780f668.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3780f668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d43ed217cf91cdca382a",
+    "url": "/static/js/main.902c21ac.chunk.js"
+  },
+  {
+    "revision": "8f467865241019a3ea1f",
+    "url": "/static/js/runtime-main.dca71b76.js"
+  },
+  {
+    "revision": "f07a081fbaa55bb4ce775601b0c670c8",
+    "url": "/static/media/bg.f07a081f.jpg"
+  },
+  {
+    "revision": "57540e46702d396dd84e34e10c116846",
+    "url": "/static/media/final.57540e46.jpg"
+  }
+]);
